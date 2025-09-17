5e76c4b2d3bd0afa54db633a89204cd55640cbef
Bruno Bussmeyer <fpemuaythai@gmail.com>
2025-09-17 10:19:52 -0300

    Adicionar arquivos de projeto.

---
+++ Aula Basica/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula_Basica
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
